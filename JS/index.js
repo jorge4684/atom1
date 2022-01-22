2f596911dee9b55686f04b3c6c4ebd9f5d8b2755
jorge4684 <js5383651@gmail.com>
2022-01-22 07:00:26 -0600

    js

---
+++ JS/index.js
@@ -0,0 +1,13 @@
+
+const range1=document.getElementById("range1");
+const range2=document.getElementById("range2");
+
+range1.addEventListener = (move) =>{
+    labelMonto.innerHTML = 0;
+    range1.innerHTML = "";
+ if( labelMonto === 0) {
+    return labelMonto.innerHTML;
+    }else{
+        return labelMonto.value.innerHTML;   
+    }; 
+}; 
